326dc29dc400f8040e08e430b73b87f88dd1d943
rn-mgl <reinb2ra@gmail.com>
2025-02-10 17:38:46 +0800

    can get folder details

--- routes/web.php
+++ routes/web.php
@@ -125,12 +125,14 @@ Route::prefix("api")->group(function() {
         Route::prefix("document")->group(function() {
             Route::controller(DocumentController::class)->group(function() {
                 Route::get("/", "index")->can("updateHR", User::class);
+                Route::post("/", "store")->can("updateHR", User::class);
             });
         });
 
         Route::prefix('document_folder')->group(function() {
             Route::controller(DocumentFolderController::class)->group(function() {
                 Route::get("/paths", "get_paths")->can("updateHR", User::class);
+                Route::get("/{document_folder}", "show")->can("updateHR", User::class);
                 Route::post("/", "store")->can("updateHR", User::class);
             });
         });
