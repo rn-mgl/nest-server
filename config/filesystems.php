b4eca53721c12d30495a4cc221e5991dc0d2517f
rn-mgl <reinb2ra@gmail.com>
2025-08-24 17:58:51 +0800

    setup ftp

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,21 @@ return [
             'throw' => false,
         ],
 
+        'ftp' => [
+            'driver' => 'ftp',
+            'host' => env('FTP_HOST'),
+            'username' => env('FTP_USERNAME'),
+            'password' => env('FTP_PASSWORD'),
+            'root' => env("FTP_ROOT")
+
+            // Optional FTP Settings...
+            // 'port' => env('FTP_PORT', 21),
+            // 'root' => env('FTP_ROOT'),
+            // 'passive' => true,
+            // 'ssl' => true,
+            // 'timeout' => 30,
+        ],
+
     ],
 
     /*
